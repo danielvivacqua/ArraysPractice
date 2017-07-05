c05be8d40f35abaf74ea4cec66b4c81aa96687e5
danielvivacqua <drvivacqua@hotmail.com>
2017-07-05 11:09:35 -0400

    Several in-class practice problems

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -10,6 +10,53 @@ namespace ArraysPractice
     {
         static void Main(string[] args)
         {
+            //declaring an array without initializing
+            //int[] myFirstArray;
+            ////declaring and initializing a second array
+            //string[] myStringArray = { "First", "second", "third", "fourth" };
+            ////declare and initialize a char array with the letters of your last name
+            //char[] myName = { 'v', 'i', 'v', 'a', 'c', 'q', 'u', 'a' };
+            ////I want my console to print three elements of my array
+            //Console.WriteLine(myName[0]);
+            //Console.WriteLine(myName[3]);
+            //Console.WriteLine(myName[1]);
+
+
+            //Create the following arrays and print using the correct index numbers:
+            //Array of Top 10 Vacation spots
+            //print the first and last vacation spots
+            //string[] vacationSpots = { "Japan", "Ireland", "Korea", "Iceland", "Cooperstown", "Australia", "Alabama", "Key West", "Jamaica", "Buffalo" };
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]);
+
+            //Array of the ages of 3 children you know
+            //print each child's age
+            //int[] childrensAges = { 13, 1, 6 };
+            //Console.WriteLine(childrensAges[0]);
+            //Console.WriteLine(childrensAges[1]);
+            //Console.WriteLine(childrensAges[2]);
+
+            //Array of GPA's on a 4.0 scale of 8 students - you don't have to print
+
+            //Array of the First Letter of 4 of your classmates' names who are sitting near you
+            //print all four on the same line
+            //char[] classFirstLetters = { 'j', 'd', 'j', 'n' };
+            //Console.WriteLine(classFirstLetters);
+            //Console.Write(classFirstLetters[0]);
+            //Console.Write(classFirstLetters[1]);
+            //Console.Write(classFirstLetters[2]);
+            //Console.Write(classFirstLetters[3]);
+
+            //Console.WriteLine("\n{0}, {1}, {2}, {3}", classFirstLetters[0], classFirstLetters[1], classFirstLetters[2], classFirstLetters[3]);
+
+
+
+
+
+
+
+
+
         }
     }
 }
